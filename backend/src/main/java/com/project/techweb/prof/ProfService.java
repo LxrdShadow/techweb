47a3ce6888444b2e236db50abfe78167c00f7386
LxrdShadow <andritiana.idealy@gmail.com>
2026-08-09 13:11:18 +0300

    feat(prof): introduce prof service

---
+++ backend/src/main/java/com/project/techweb/prof/ProfService.java
@@ -0,0 +1,34 @@
+package com.project.techweb.prof;
+
+import java.util.List;
+import org.springframework.stereotype.Service;
+
+@Service
+public class ProfService {
+
+    private final ProfRepository profRepository;
+
+    public ProfService(ProfRepository profRepository) {
+        this.profRepository = profRepository;
+    }
+
+    public Prof findByCodeprof(int codeprof) {
+        return profRepository.findById(codeprof).orElse(null);
+    }
+
+    public List<Prof> findAll() {
+        return profRepository.findAll();
+    }
+
+    public Prof createProf(Prof prof) {
+        return profRepository.save(prof);
+    }
+
+    public Prof updateProf(Prof prof) {
+        return profRepository.save(prof);
+    }
+
+    public void deleteProf(int codeprof) {
+        profRepository.deleteById(codeprof);
+    }
+}
